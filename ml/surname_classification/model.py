06699f1d9022206c5d0eb6e94dc23f0bc05cde64
Brave-Oni <braveoni.ml@gmail.com>
2022-06-13 02:12:32 +1000

    Add surname classifier

---
+++ ml/surname_classification/model.py
@@ -0,0 +1,204 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+
+
+def column_gather(y_out, x_lengths):
+    x_lengths = x_lengths.long().detach().cpu().numpy() - 1
+
+    out = []
+    for batch_index, column_index in enumerate(x_lengths):
+        out.append(y_out[batch_index, column_index])
+
+    return torch.stack(out)
+
+
+class Vocabulary(object):
+    def __init__(self, token_to_idx=None):
+
+        if token_to_idx is None:
+            token_to_idx = {}
+        self._token_to_idx = token_to_idx
+
+        self._idx_to_token = {idx: token
+                              for token, idx in self._token_to_idx.items()}
+
+    def to_serializable(self):
+        return {'token_to_idx': self._token_to_idx}
+
+    @classmethod
+    def from_serializable(cls, contents):
+        return cls(**contents)
+
+    def add_token(self, token):
+        if token in self._token_to_idx:
+            index = self._token_to_idx[token]
+        else:
+            index = len(self._token_to_idx)
+            self._token_to_idx[token] = index
+            self._idx_to_token[index] = token
+        return index
+
+    def add_many(self, tokens):
+        return [self.add_token(token) for token in tokens]
+
+    def lookup_token(self, token):
+        return self._token_to_idx[token]
+
+    def lookup_index(self, index):
+        if index not in self._idx_to_token:
+            raise KeyError("the index (%d) is not in the Vocabulary" % index)
+        return self._idx_to_token[index]
+
+    def __str__(self):
+        return "<Vocabulary(size=%d)>" % len(self)
+
+    def __len__(self):
+        return len(self._token_to_idx)
+
+
+class SequenceVocabulary(Vocabulary):
+    def __init__(self, token_to_idx=None, unk_token="<UNK>",
+                 mask_token="<MASK>", begin_seq_token="<BEGIN>",
+                 end_seq_token="<END>"):
+
+        super(SequenceVocabulary, self).__init__(token_to_idx)
+
+        self._mask_token = mask_token
+        self._unk_token = unk_token
+        self._begin_seq_token = begin_seq_token
+        self._end_seq_token = end_seq_token
+
+        self.mask_index = self.add_token(self._mask_token)
+        self.unk_index = self.add_token(self._unk_token)
+        self.begin_seq_index = self.add_token(self._begin_seq_token)
+        self.end_seq_index = self.add_token(self._end_seq_token)
+
+    def to_serializable(self):
+        contents = super(SequenceVocabulary, self).to_serializable()
+        contents.update({'unk_token': self._unk_token,
+                         'mask_token': self._mask_token,
+                         'begin_seq_token': self._begin_seq_token,
+                         'end_seq_token': self._end_seq_token})
+        return contents
+
+    def lookup_token(self, token):
+        if self.unk_index >= 0:
+            return self._token_to_idx.get(token, self.unk_index)
+        else:
+            return self._token_to_idx[token]
+
+
+class SurnameVectorizer(object):
+    def __init__(self, char_vocab, nationality_vocab):
+        self.char_vocab = char_vocab
+        self.nationality_vocab = nationality_vocab
+
+    def vectorize(self, surname, vector_length=-1):
+        indices = [self.char_vocab.begin_seq_index]
+        indices.extend(self.char_vocab.lookup_token(token)
+                       for token in surname)
+        indices.append(self.char_vocab.end_seq_index)
+
+        if vector_length < 0:
+            vector_length = len(indices)
+
+        out_vector = np.zeros(vector_length, dtype=np.int64)
+        out_vector[:len(indices)] = indices
+        out_vector[len(indices):] = self.char_vocab.mask_index
+
+        return out_vector, len(indices)
+
+    @classmethod
+    def from_dataframe(cls, surname_df):
+        char_vocab = SequenceVocabulary()
+        nationality_vocab = Vocabulary()
+
+        for index, row in surname_df.iterrows():
+            for char in row.surname:
+                char_vocab.add_token(char)
+            nationality_vocab.add_token(row.nationality)
+
+        return cls(char_vocab, nationality_vocab)
+
+    @classmethod
+    def from_serializable(cls, contents):
+        char_vocab = SequenceVocabulary.from_serializable(contents['char_vocab'])
+        nat_vocab =  Vocabulary.from_serializable(contents['nationality_vocab'])
+
+        return cls(char_vocab=char_vocab, nationality_vocab=nat_vocab)
+
+    def to_serializable(self):
+        return {'char_vocab': self.char_vocab.to_serializable(),
+                'nationality_vocab': self.nationality_vocab.to_serializable()}
+
+
+class SurnameClassifier(nn.Module):
+    def __init__(self, batch_first=True, padding_idx=0):
+        super(SurnameClassifier, self).__init__()
+
+        self.emb = nn.Embedding(
+            num_embeddings=100,
+            embedding_dim=300,
+            padding_idx=padding_idx
+        )
+
+        self.rnn = nn.LSTM(
+            input_size=300,
+            hidden_size=256,
+            batch_first=batch_first,
+            dropout=.3
+        )
+
+        self.fc1 = nn.Linear(
+            in_features=256,
+            out_features=256
+        )
+
+        self.fc2 = nn.Linear(
+            in_features=256,
+            out_features=18
+        )
+
+    @staticmethod
+    def init_hidden(batch_size):
+        h0 = torch.zeros((1, batch_size, 256)).cuda()
+        c0 = torch.zeros((1, batch_size, 256)).cuda()
+
+        return h0, c0
+
+    def forward(self, x_in, hidden, x_lengths=None, apply_softmax=False):
+        x_embedded = self.emb(x_in)
+        y_out, hidden = self.rnn(x_embedded, hidden)
+
+        if x_lengths is not None:
+            y_out = column_gather(y_out, x_lengths)
+        else:
+            y_out = y_out[:, -1, :]
+
+        y_out = F.relu(self.fc1(F.dropout(y_out, 0.5)))
+        y_out = self.fc2(F.dropout(y_out, 0.5))
+
+        if apply_softmax:
+            y_out = F.softmax(y_out, dim=1)
+
+        return y_out, hidden
+
+
+def predict_nationality(surname, classifier, vectorizer):
+    vectorized_surname, vec_length = vectorizer.vectorize(surname)
+    vectorized_surname = torch.tensor(vectorized_surname).unsqueeze(dim=0)
+    vec_length = torch.tensor([vec_length], dtype=torch.int64)
+
+    h = classifier.init_hidden(1)
+
+    result, h = classifier(vectorized_surname, h, vec_length, apply_softmax=True)
+    probability_values, indices = result.max(dim=1)
+
+    index = indices.item()
+    prob_value = probability_values.item()
+
+    predicted_nationality = vectorizer.nationality_vocab.lookup_index(index)
+
+    return {'nationality': predicted_nationality, 'probability': prob_value, 'surname': surname}
