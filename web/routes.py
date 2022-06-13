7a72ef9befc47a5e7069d239a80fc01fbffdb3cc
Brave-Oni <braveoni.ml@gmail.com>
2022-06-13 20:03:55 +1000

    Rebase project

--- web/routes.py
+++ web/routes.py
@@ -25,7 +25,7 @@ def home():
     prediction = ""
     # if request.method == 'POST':
     #     model = ToxicModel()
-    #     model.state_dict(torch.load("ml/to_upload/Toxificator-512-model.pth", map_location=torch.device('cpu')))
+    #     model.state_dict(torch.load("ml/to_upload/model.pth", map_location=torch.device('cpu')))
     #     model.eval()
     #     user_input = request.form.get('user_input_text')
     #     prediction = ToxicPredict(model, user_input)
@@ -35,12 +35,12 @@ def home():
         model = NationModel()
         model.state_dict(
             torch.load(
-                "ml/to_upload/Nation_model.pth", map_location=torch.device("cpu")
+                "ml/to_upload/model.pth", map_location=torch.device("cpu")
             )
         )
         model.eval()
         user_input = request.form.get("user_input_text")
-        with open("ml/models/vectorizer_for_Nation.json") as file:
+        with open("ml/models/vectorizer.json") as file:
             content = json.load(file)
         vectorizer = SurnameVectorizer.from_serializable(content)
         prediction = NationPredict(user_input, model, vectorizer)
